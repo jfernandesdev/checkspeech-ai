64a583e281b7f0efaa13c59c9552b75877caeced
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-21 12:18:48 -0300

    STYLE: hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import { Navbar } from '../components/Navbar'
+import { Hero } from '../components/Hero'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
 
       <main>
         <Navbar />
+        <Hero />
         <h1>Hello World!</h1>
       </main>
     </>
