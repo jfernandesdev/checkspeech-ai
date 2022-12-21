444a4ee7e3c71de9851c87f99a54baa5b7f1dfdb
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-21 10:26:17 -0300

    CHORE: setup initial with nextjs, typescript and sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CheckSpeech AI</title>
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </>
+  )
+}
